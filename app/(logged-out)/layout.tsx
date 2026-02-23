b76624194eb2d2dfcee347dd41f6eea6b9e8b79f
CodyJReed <cody2j2reed@gmail.com>
2026-02-23 15:54:51 -0500

    add new hoisted layout; update root page; assign base heading font-sizes

---
+++ app/(logged-out)/layout.tsx
@@ -0,0 +1,11 @@
+interface ComponentProps {
+  children?: React.ReactNode;
+}
+
+export default function LoggedOutLayout({ children }: ComponentProps) {
+  return (
+    <div className="flex flex-col items-center min-h-screen p-24 justify-center gap-y-4">
+      {children}
+    </div>
+  );
+}
