46a235cc5066f22d186477c98916376626f2f7fb
CodyJReed <cody2j2reed@gmail.com>
2026-02-24 13:03:28 -0500

    install card from shadcn; add login and signup pages; complete login boilerplate

---
+++ app/(logged-out)/login/page.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { PersonStandingIcon } from "lucide-react";
+import Link from "next/link";
+
+export default function Login() {
+  return (
+    <>
+      <PersonStandingIcon size={50} />
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Login to your SupportMe account</CardDescription>
+        </CardHeader>
+        <CardContent>login form</CardContent>
+        <CardFooter className="justify-between">
+          Dont' have an account{" "}
+          <Button asChild variant="outline" size="xs">
+            <Link href="/signup">Sign up</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
+}
